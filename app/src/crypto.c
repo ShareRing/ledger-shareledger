418760e5556933df3e4fb0ffb0e82b6db324a43d
Carlos Medeiros <carlos.medeiros@zondax.ch>
2024-02-22 12:55:42 +0000

    update keccak hash to new sdk functions

--- app/src/crypto.c
+++ app/src/crypto.c
@@ -186,11 +186,7 @@ zxerr_t crypto_fillAddress(uint8_t *buffer, uint16_t buffer_len, uint16_t *addrR
         }
 
         case BECH32_ETH: {
-            cx_sha3_t ctx;
-            if (cx_keccak_init_no_throw(&ctx, 256) != CX_OK) {
-                return zxerr_unknown;
-            }
-            CHECK_CX_OK(cx_hash_no_throw((cx_hash_t *)&ctx, CX_LAST, uncompressedPubkey+1, sizeof(uncompressedPubkey)-1, hashed1_pk, sizeof(hashed1_pk)));
+            CHECK_CX_OK(cx_keccak_256_hash(uncompressedPubkey+1, sizeof(uncompressedPubkey)-1, hashed1_pk));
             CHECK_ZXERR(bech32EncodeFromBytes(addr, buffer_len - PK_LEN_SECP256K1, bech32_hrp, hashed1_pk + 12, sizeof(hashed1_pk) - 12, 1, BECH32_ENCODING_BECH32));
             break;
         }
